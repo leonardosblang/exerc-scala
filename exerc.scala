bbb009eead186e95194706bc05c5abcf7e3be6d5
Leonardo S.B. Lang <49520669+leonardosblang@users.noreply.github.com>
2022-04-11 19:42:45 -0300

    Add files via upload

---
+++ exerc.scala
@@ -0,0 +1,22 @@
+
+var num1 = scala.io.StdIn.readInt()
+var num2 = scala.io.StdIn.readInt()
+
+var arr = new Array[Int](20)
+
+for(i <- 0 to arr.length-1)
+{
+  if(i%2==0)
+  {
+    arr(i) = num1*i
+  }
+  else
+  {
+    arr(i) = num2*i
+  }
+}
+
+for(i <- 0 to arr.length-1)
+{
+  print(arr(i)+" ")
+}
